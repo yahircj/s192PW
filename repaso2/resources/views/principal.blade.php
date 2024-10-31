8495298400f1a0a64d0eb6fc32f3d9119c054fab
yahircj <130490177+yahircj@users.noreply.github.com>
2024-10-31 10:45:02 -0600

    Agregar noticia literaria y preparacion de los input para los campos

--- repaso2/resources/views/principal.blade.php
+++ repaso2/resources/views/principal.blade.php
@@ -3,5 +3,18 @@
 @section('contenido')
     
 
+<center>
+<div style="width: 80%; max-width: 800px; background-color: #fff; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); overflow: hidden;">
+    <div style="background-color: #000; color: #fff; padding: 20px; font-size: 1.5em; text-align: center;">
+        Ann Patchett: "Para un escritor, un libro malo es mucho más inspirador que uno bueno"
+    </div>
+    <div style="padding: 20px; font-size: 1em; color: #333;">
+        La autora estadounidense publica 'Tom Lake', una novela sobre las expectativas, el amor y la idea que nos imponen de felicidad. "De jóvenes todos creemos haber nacido en la familia equivocada".
+        <br>
+        <br>
+        La novela más reciente de Ann Patchett, Tom Lake, transcurre durante la pandemia, pero no trata sobre la pandemia: el encierro es lo que propicia el regreso de las tres hijas de Lara Nelson a la granja familiar y le ofrece el tiempo para hablarles a sus hijas de su pasado y de una arrebatada historia de amor que tuvo con Peter Duke, que años después acabaría recogiendo un Oscar de la mano de Viola Davis. La pandemia llegó por azar: "La idea del libro se me ocurrió antes de la pandemia, iba a ser el mismo libro pero situado en verano, la época en la que vuelves a casa a trabajar si has crecido en una granja, pero la pandemia le daba otro nivel de intimidad", explica la escritora por videollamada.
+    </div>
+</div>
+</center>
 
 @endsection
--- repaso2/resources/views/registroLibro.blade.php
+++ repaso2/resources/views/registroLibro.blade.php
@@ -8,13 +8,34 @@
       <h5 class="card-title">Formulario</h5>
       <form>
         <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <label for="exampleInputEmail1" class="form-label">ISBN</label>
+          <input type="text" class="form-control" name="txtisbn" value="{{ old('txtisbn') }}">>
         </div>
         <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1">
+          <label for="exampleInputPassword1" class="form-label">TITULO</label>
+          <input type="text" class="form-control" name="txttitulo" value="{{ old('txttitulo') }}">>
         </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">AUTOR</label>
+          <input type="text" class="form-control" name="txtautor" value="{{ old('txtautor') }}">>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">PÁGINAS</label>
+          <input type="text" class="form-control" name="txtpaginas" value="{{ old('txtpaginas') }}">>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">AÑO</label>
+          <input type="text" class="form-control" name="txtano" value="{{ old('txtano') }}">>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">EDITORIAL</label>
+          <input type="text" class="form-control" name="txteditorial" value="{{ old('txteditorial') }}">>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">EMAIL DE EDITORIAL</label>
+          <input type="text" class="form-control" name="txtemail" value="{{ old('txtemail') }}">>
+        </div>
+      
 
       <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
